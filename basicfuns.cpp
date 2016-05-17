20d793efdd8a3d707a16aa409bf16db64ae2ff9c
thomasjmurphy <tjmurphy@andrew.cmu.edu>
2016-05-16 22:11:36 -0400

    Create basicfuns.cpp
    rowReduce

---
+++ basicfuns.cpp
@@ -0,0 +1,77 @@
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+void swap(double &a, double &b)
+{
+    if(&a == &b){return;}
+a = a + b;
+b = a - b;
+a = a - b;
+}
+
+void rowDivide(vector<double> &vec, int index)
+{
+    double div = vec[index];
+    for(int i=0;i<vec.size();i++)
+    {
+        vec[i] = vec[i]/div;
+    }
+}
+
+void printMat(vector<vector<double>> &mat)
+{
+    int cols = mat[0].size();
+    int rows = mat.size();
+    for(int i=0;i<rows;i++)
+    {
+        for(int j=0;j<cols;j++)
+        {
+            cout << mat[i][j];
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+
+void rowReduce(vector<vector<double>> &mat)
+{
+    int pivot = 0;
+    int nextpivot = 0;
+    int cols = mat[0].size();
+    int rows = mat.size();
+    for(int i=0;i<cols;i++)
+    {
+        nextpivot = pivot;
+        while(mat[nextpivot][i] == 0 && nextpivot < rows)
+        {
+            nextpivot++;
+        }
+        
+        if(mat[nextpivot][i] != 0 && nextpivot < rows)
+        {
+            for(int k = i;k<cols;k++)
+            {
+                swap(mat[nextpivot][k],mat[pivot][k]);
+            }
+            printMat(mat); cout << endl;
+            rowDivide(mat[pivot],i);
+            printMat(mat); cout << endl;
+            for(int k = pivot+1;k<rows;k++)
+            {
+                double multiplier = mat[k][i];
+                for(int l = i;l<cols;l++)
+                {
+                    mat[k][l] = mat[k][l] - multiplier*mat[pivot][l];
+                }
+                printMat(mat);
+            }
+            
+            pivot++;
+        }
+        if(pivot == rows){break;}
+    }
+}
+
